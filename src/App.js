34ef7e670ef34e3297752314bea506bceb82a6de
ynotstartups <ynotstartups@gmail.com>
2019-04-18 08:02:48 +0100

    formatting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Search from '@material-ui/icons/Search';
-import { useDebounce } from 'use-debounce';
-import InputAdornment from '@material-ui/core/InputAdornment';
-
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Search from "@material-ui/icons/Search";
+import { useDebounce } from "use-debounce";
+import InputAdornment from "@material-ui/core/InputAdornment";
 
 const SEARCH_ARTISTS = query => {
   return gql`
@@ -46,22 +45,29 @@ const SEARCH_ARTISTS = query => {
 };
 
 const App = ({ onDogSelected }) => {
-
   const [query, setQuery] = useState("tiger");
   const [debouncedQuery] = useDebounce(query, 1000);
-
 
   window.setQuery = setQuery;
 
   return (
     <div style={{ padding: 20 }}>
-      <TextField fullWidth id="input-with-icon-grid" label="Search for Artist" onChange={(e) => { setQuery(e.target.value) }} style={{ paddingBottom: 20 }} InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <Search />
-          </InputAdornment>
-        ),
-      }} />
+      <TextField
+        fullWidth
+        id="input-with-icon-grid"
+        label="Search for Artist"
+        onChange={e => {
+          setQuery(e.target.value);
+        }}
+        style={{ paddingBottom: 20 }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          )
+        }}
+      />
 
       <Query query={SEARCH_ARTISTS(debouncedQuery)}>
         {({ loading, error, data }) => {
@@ -69,11 +75,16 @@ const App = ({ onDogSelected }) => {
           if (error) return `Error! ${error.message}`;
           return (
             <Grid container spacing={32}>
-
               {data.search.edges.map(({ node }) => {
                 const { displayLabel, imageUrl, href, bio } = node;
                 return (
-                  <Grid item xs={12} sm={6} md={3} lg={2} justify="space-evenly"
+                  <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    md={3}
+                    lg={2}
+                    justify="space-evenly"
                   >
                     <Card>
                       <CardMedia
@@ -85,34 +96,38 @@ const App = ({ onDogSelected }) => {
                         <Typography gutterBottom variant="h5" component="h2">
                           {displayLabel}
                         </Typography>
-                        <Typography component="p">
-                          {bio}
-                        </Typography>
+                        <Typography component="p">{bio}</Typography>
                       </CardContent>
                       <CardActions>
-                        <Button size="small" color="primary" onClick={() => { window.open(`https://www.artsy.net${href}`) }} >
+                        <Button
+                          size="small"
+                          color="primary"
+                          onClick={() => {
+                            window.open(`https://www.artsy.net${href}`);
+                          }}
+                        >
                           Learn More
-                  </Button>
+                        </Button>
                       </CardActions>
                     </Card>
                   </Grid>
-
-                )
+                );
               })}
             </Grid>
           );
         }}
-      </Query></div >
-  )
+      </Query>
+    </div>
+  );
 };
 
 const styles = {
   card: {
-    maxWidth: 345,
+    maxWidth: 345
   },
   media: {
-    height: 140,
-  },
+    height: 140
+  }
 };
 
 export default App;
