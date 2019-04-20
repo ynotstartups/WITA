7082b063ba587081b13bbc3602391bd7b35bb49a
ynotstartups <ynotstartups@gmail.com>
2019-04-20 16:42:21 +0100

    support infinite scrolling

--- src/Pages/PageArtistGallery.js
+++ src/Pages/PageArtistGallery.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import gql from "graphql-tag";
-import { Query } from "react-apollo";
 import { Typography } from "@material-ui/core";
 
 import Header from "../Components/Header/Header";
 import ArtworkGallery from "../Components/ArtworkGallery/ArtworkGallery";
+import { withApollo } from "react-apollo";
 
 const ArtistImages = gql`
-  query ArtistImages($id: String!) {
+  query ArtistImages($id: String!, $page: Int!) {
     artist(id: $id) {
       displayLabel
-      artworks(size: 50) {
+      artworks(page: $page) {
         image {
           width
           height
@@ -20,38 +20,124 @@ const ArtistImages = gql`
     }
   }
 `;
+const initialState = {
+  photos: [],
+  initFetchDone: false,
+  displayLabel: null,
+  page: 1,
+  ended: false
+};
 
-const PageArtistGallery = ({ savedArtists, match }) => {
+function reducer(state, action) {
+  switch (action.type) {
+    case "append":
+      return {
+        photos: state.photos.concat(action.photos),
+        displayLabel: action.displayLabel,
+        initFetchDone: true,
+        page: state.page + 1,
+        ended: action.ended
+      };
+    default:
+      throw new Error();
+  }
+}
+
+function appendPhotos(displayLabel, photos, ended) {
+  return {
+    type: "append",
+    photos,
+    ended,
+    displayLabel
+  };
+}
+
+const useInfiniteScroll = callback => {
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!isFetching) return;
+    callback();
+  }, [isFetching]);
+
+  function handleScroll() {
+    // fetch on only 1 more screen of content left
+    if (
+      window.innerHeight + document.documentElement.scrollTop <
+        document.documentElement.offsetHeight - window.innerHeight ||
+      isFetching
+    )
+      return;
+    setIsFetching(true);
+  }
+
+  return [isFetching, setIsFetching];
+};
+
+const PageArtistGallery = ({ savedArtists, match, client }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [isFetching, setIsFetching] = useInfiniteScroll(async () => {
+    if (state.ended) {
+      // for stopping the useInfiniteScroll callback being called
+      setIsFetching(true);
+      return;
+    }
+
+    await fetchPhotos();
+    setIsFetching(false);
+  });
+
+  const fetchPhotos = async () => {
+    const { data } = await client.query({
+      query: ArtistImages,
+      variables: { id: match.params.id, page: state.page }
+    });
+
+    const { artworks, displayLabel } = data.artist;
+    const photos = artworks.map(({ image }) => ({
+      width: image.width,
+      height: image.height,
+      src: image.url
+    }));
+
+    let ended = false;
+    if (artworks.length === 0) {
+      ended = true;
+    }
+
+    dispatch(appendPhotos(displayLabel, photos, ended));
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
   return (
     <>
       <Header />
-      {/* TODO make a content wrapper component */}
       <div style={{ padding: 32 }}>
-        <Query query={ArtistImages} variables={{ id: match.params.id }}>
-          {({ loading, error, data }) => {
-            if (loading) return "Loading...";
-            if (error) return `Error! ${error.message}`;
-
-            const { artworks, displayLabel } = data.artist;
-            const photos = data.artist.artworks.map(({ image }) => ({
-              width: image.width,
-              height: image.height,
-              src: image.url
-            }));
-
-            return (
-              <>
-                <Typography variant="h4" gutterBottom>
-                  Artwork Gallery for {displayLabel}
-                </Typography>
-                <ArtworkGallery photos={photos} />
-              </>
-            );
-          }}
-        </Query>
+        {state.initFetchDone ? (
+          <>
+            <Typography variant="h4" gutterBottom>
+              Artwork Gallery for {state.displayLabel}
+            </Typography>
+            <ArtworkGallery photos={state.photos} />
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
+        {!state.ended && isFetching && <div>Loading...</div>}
+        {state.ended && (
+          <div>That's all the artworks in Artsy for this artist...</div>
+        )}
       </div>
     </>
   );
 };
 
-export default PageArtistGallery;
+export default withApollo(PageArtistGallery);
