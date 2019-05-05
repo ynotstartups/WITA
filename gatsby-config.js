535ed9cc85a3d6b817e25ddf59e689b51405187d
ynotstartups <ynotstartups@gmail.com>
2019-05-05 23:45:23 +0100

    PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,24 +21,19 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `WhoIsThisArtists?`,
+        short_name: `WITA?`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#ffffff`,
+        theme_color: `#d2a32d`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    `gatsby-plugin-typescript`,
+    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-source-graphql",
       options: {
