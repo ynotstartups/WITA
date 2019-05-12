8bce6f61a8d1d8a48eb9377bf0f38e9a6a009e14
ynotstartups <ynotstartups@gmail.com>
2019-05-12 21:08:00 +0100

    support safari and IE

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -20,3 +20,11 @@ export const wrapRootElement = ({ element }) => {
     </ApolloProvider>
   )
 }
+
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`)
+    console.log(`# IntersectionObserver is polyfilled!`)
+  }
+}
